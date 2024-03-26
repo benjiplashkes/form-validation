04ffd96b8eba0ca6b116f63c2c8d2e483fe30d59
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-03-26 23:53:08 +0200

    Add styles to valid and invalid input states.
    add the valid states to inputs
    add the invalid state to inputs
    add focus state to inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,18 @@
-const form = document.querySelector('#createAccountForm')
-const inputs = form.querySelectorAll('input')
+const inputs = document.querySelectorAll("form input");
 
-const validateForm = (e)=>{
-    //error displaying
-    const errorDiv = document.querySelector('div.error')
-    const error = (text, element)=>{
-        console.log(text)
-        console.log(element.parentElement)
-        const div = document.createElement('div')
-        div.classList.add('error')
-        div.textContent = text
-        element.parentElement.appendChild(div)
-        element.classList.add('error')
-    }
-    
-    // Check password Validation
-    const checkPassword = ()=>{
-        const pass = form.querySelector("#password")
-        const passConfirm = form.querySelector("#password-confirm")
-        
-        errorDiv?.remove() // reset error state
-        
-        if(!pass.value) return  false
-        if(!passConfirm.value) return  false
-        if(pass.value !== passConfirm.value) {
-            error("passwords do not match", passConfirm)
-            return false
+inputs.forEach((input) => {
+  addEventListener("change", (e) => {
+    setTimeout(()=>{
+        input = e.target
+        input.parentElement.classList.remove('valid');
+        input.parentElement.classList.remove('invalid')
+        if (input.validity.valid){
+           return input.parentElement.classList.add('valid')
         }
-             
-        if(pass.value === passConfirm.value){
-            pass.classList.add('valid')
-            passConfirm.classList.add('valid')
-            return true
-        }
-        return 
-    }
-    const validateFields = () =>{
-        return true
-    }
-    const styleValidFields = ()=>{
-        const input = e.target
-      
-            console.log(input.parentElement)
-            if(input.validity.valid && !input.indeterminate) input.parentElement.classList.add('valid')
+        return input.parentElement.classList.add('invalid')
         
-    }
-    styleValidFields()
-    valid = checkPassword() ? validateFields() : false
-    return valid 
-}
-form.addEventListener("change", validateForm)
-form.addEventListener("submit", (e)=>{
-    e.preventDefault()
-
-    if(validateForm())form.submit()
-    return
-})
+    }, 500)
+  });
+});
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("password-confirm");
