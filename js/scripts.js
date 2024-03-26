da1f9e48ffce7e16b22f29d2e646b4715c126b6b
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-03-26 15:15:34 +0200

    Add JS validation logic
    add event listeners and logic to validate the confim password field
    matches the password field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-document.write("Javascript")
+const inputs = document.querySelectorAll('input')
+const form = document.querySelector('form')
+const submit = document.querySelector('[type="submit"]')
+const firstname = document.querySelector('#firstname')
+const lastname = document.querySelector('#lastname')
+const email = document.querySelector('#email')
+const phone = document.querySelector('#tel')
+const password = document.querySelector('#password')
+const passwordConfirm = document.querySelector('#password-confirm')
+
+
+passwordConfirm.addEventListener("change", (e)=>{
+    let passwordValue = password.value
+    let passwordConfirmValue = passwordConfirm.value
+    let errorDiv = document.createElement('div')
+    errorDiv.classList.add('error')
+    if(!passwordValue){
+        password.classList.add("error")
+        errorDiv.textContent = "Please fill in your password"
+        password.parentElement.appendChild(errorDiv)  
+        return  
+    }
+    if(!passwordConfirmValue){
+        passwordConfirm.classList.add("error")
+        errorDiv.textContent = "Please fill in your password"
+        passwordConfirm.parentElement.appendChild(errorDiv)    
+        return
+    }
+    if(passwordValue !== passwordConfirmValue){
+        e.preventDefault()
+        passwordConfirm.classList.add('error')
+        errorDiv.textContent = "Passwords do not match"
+        passwordConfirm.parentElement.appendChild(errorDiv)
+        password.focus()
+    }
+    if(passwordConfirmValue === passwordValue){
+        password.classList.remove('error')
+        passwordConfirm.classList.remove('error')
+        return
+    }
+})
+
