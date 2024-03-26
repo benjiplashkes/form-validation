ed882965272a09c058a3ee43e3a32d976a7487fc
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-03-27 00:23:10 +0200

    Add Password Validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,43 @@
 const inputs = document.querySelectorAll("form input");
 
+//style inputs by validation
+
 inputs.forEach((input) => {
   addEventListener("change", (e) => {
-    setTimeout(()=>{
-        input = e.target
-        input.parentElement.classList.remove('valid');
-        input.parentElement.classList.remove('invalid')
-        if (input.validity.valid){
-           return input.parentElement.classList.add('valid')
-        }
-        return input.parentElement.classList.add('invalid')
-        
-    }, 500)
+    input = e.target;
+
+    const styleValidatedInputs = () => {
+      input.parentElement.classList.remove("valid");
+      input.parentElement.classList.remove("invalid");
+      if (input.validity.valid) {
+        if (input.id === "passwordconfirm") return;
+        return input.parentElement.classList.add("valid");
+      }
+      return input.parentElement.classList.add("invalid");
+    };
+    const validatePassword = () => {
+      const passwordFieldsDiv = document.querySelector("div.password-fields");
+      document.querySelector('div.error')?.remove()
+      if (input.id !== "password-confirm") return;
+      if (password.value === passwordConfirm.value) {
+        password.parentElement.classList.add("valid");
+        passwordConfirm.parentElement.classList.add("valid");
+        return;
+      }
+      if (password.value !== passwordConfirm.value) {
+        password.parentElement.classList.add("invalid");
+        passwordConfirm.parentElement.classList.add("invalid");
+        const div = document.createElement("div");
+
+        div.classList.add("error");
+        div.textContent = `passwords do not match`;
+        passwordFieldsDiv.appendChild(div);
+        passwordConfirm.value = ""
+        password.focus()
+      }
+    };
+    styleValidatedInputs();
+    validatePassword();
   });
 });
 const password = document.getElementById("password");
